79d1f8d5032b558ff07745d46d67346c5d8c8e0c
LBoalich <lkboalich@gmail.com>
2022-12-29 16:43:55 -0500

    Adding basic navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
 
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     
-      </header>
+      <nav class="navbar">
+        <div class="home">
+          <a href="">redditMinimal</a> {/* add href link so returns to home state */}
+        </div>
+        <div class="searchBar">
+          <input type="text" placeholder="Search.." /> {/* Add search icon */}
+        </div>
+        <div class="logo">
+          <img src="" alt="" /> {/* add reddit logo.  On click returns to home */}
+        </div>
+      </nav>
     </div>
   );
 }
