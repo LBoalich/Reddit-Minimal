d2802a2ee6ee6a67d7c04517c81d36c64711fd66
LBoalich <lkboalich@gmail.com>
2023-01-01 17:58:58 -0500

    Fixing NavBar hide on scroll

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
+import Navbar from './components/NavBar';
 
 function App() {
-  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-  var prevScrollpos = window.pageYOffset;
-  useEffect(() => {
-    const handleScroll = event => {
-      console.log('window.scrollY', window.scrollY);
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-      } else {
-        document.getElementById("navbar").style.top = "-50px";
-      }
-      prevScrollpos = currentScrollPos;
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+  
   return (
     <div className="App">
-      <nav class="navbar">
-        <div class="home">
-          <a href="">redditMinimal</a> {/* add href link so returns to home state */}
-        </div>
-        <div class="searchBar">
-          <input type="text" placeholder="Search.." /> {/* Add search icon */}
-        </div>
-        <div class="logo">
-          <img src="" alt="" /> {/* add reddit logo.  On click returns to home */}
-        </div>
-      </nav>
+      <Navbar />
       <div class="features" >
         <div class="posts">
           <p class="post">Posts go Here</p> {/* Map over posts */}
---
+++ src/components/NavBar.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { debounce } from '../utilities/helpers';
+import "./NavBar.css";
+
+const Navbar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = debounce(() => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 60) || currentScrollPos < 10);
+
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+
+  }, [prevScrollPos, visible, handleScroll]);
+
+  const navbarStyles = {
+    position: 'fixed',
+    height: '50px',
+    width: '100%',
+    backgroundColor: 'grey',
+    textAlign: 'center',
+    transition: 'top 500ms'
+  }
+
+  return (
+    <div style={{ ...navbarStyles, top: visible ? '0' : '-50px' }}>
+      <nav class="navbar">
+        <div class="home">
+          <a href="">redditMinimal</a> {/* add href link so returns to home state */}
+        </div>
+        <div class="searchBar">
+          <input type="text" placeholder="Search.." /> {/* Add search icon */}
+        </div>
+        <div class="logo">
+          <img src="" alt="" /> {/* add reddit logo.  On click returns to home */}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
