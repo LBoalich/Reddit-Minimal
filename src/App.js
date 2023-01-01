062b294b7f42f817025e5bbcbc2884fcc1303aa0
LBoalich <lkboalich@gmail.com>
2023-01-01 16:11:11 -0500

    Adding basic component layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-};
-
-
 function App() {
+  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+  var prevScrollpos = window.pageYOffset;
+  useEffect(() => {
+    const handleScroll = event => {
+      console.log('window.scrollY', window.scrollY);
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-50px";
+      }
+      prevScrollpos = currentScrollPos;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="App">
       <nav class="navbar">
@@ -28,6 +36,19 @@ function App() {
           <img src="" alt="" /> {/* add reddit logo.  On click returns to home */}
         </div>
       </nav>
+      <div class="features" >
+        <div class="posts">
+          <p class="post">Posts go Here</p> {/* Map over posts */}
+          <p class="post">Next Posts goes here</p>
+          <p class="post">And another one</p>
+        </div>
+        <div class="subreddits">
+          <h1>Subreddits</h1>
+          <p class="subreddit">Sub 1</p> {/* Map over subreddits */}
+          <p class="subreddit">Sub 2</p> 
+          <p class="subreddit">Sub 3</p>
+        </div>
+      </div>
     </div>
   );
 }
